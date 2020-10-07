cbbe026376a7b600dfa7879e69889b9418032afa
Басан <basan.info.2012@gmail.com>
2020-10-07 21:26:29 +0300

    Инициализация базы данных, добавление маппингов

--- src/main/java/ru/lidzhiev/bankaccountmanager/Entity/Account.java
+++ src/main/java/ru/lidzhiev/bankaccountmanager/Entity/Account.java
@@ -1,21 +1,35 @@
 package ru.lidzhiev.bankaccountmanager.Entity;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import static javax.persistence.GenerationType.IDENTITY;
+
 @Entity
-@Table(name = "Account")
+@Table(name = "ACCOUNT")
 public class Account {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
     private int id;
     private int amount;
+
+    public int getId() {
+        return id;
+    }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    @Id
-    public int getId() {
-        return id;
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
     }
 }
